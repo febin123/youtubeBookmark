1dfeaa35b347d1bf9784a531cea7a21c111b7a66
febin123 <febsep96@gmail.com>
2024-04-18 13:52:20 +0530

    added popup box which identifies youtube channel

--- popup.js
+++ popup.js
@@ -29,4 +29,22 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL();
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+  
+    const currentVideo = urlParameters.get("v");
+  
+    if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
+      chrome.storage.sync.get([currentVideo], (data) => {
+        const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
+  
+        viewBookmarks(currentVideoBookmarks);
+      });
+    } else {
+      const container = document.getElementsByClassName("container")[0];
+  
+      container.innerHTML = '<div class="title">This is not a youtube video page.</div>';
+    }
+  });
