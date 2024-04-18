5b34ef4b7e6d5c8d93c204a92fdfc10f7312135f
febin123 <febsep96@gmail.com>
2024-04-18 13:49:28 +0530

    Created addNewBookmark in popup

--- popup.js
+++ popup.js
@@ -1,5 +1,25 @@
 // adding a new bookmark row to the popup
-const addNewBookmark = () => {};
+import { getActiveTabURL } from "./utils.js";
+const addNewBookmark = (bookmarks, bookmark) => {
+    const bookmarkTitleElement = document.createElement("div");
+    const controlsElement = document.createElement("div");
+    const newBookmarkElement = document.createElement("div");
+  
+    bookmarkTitleElement.textContent = bookmark.desc;
+    bookmarkTitleElement.className = "bookmark-title";
+    controlsElement.className = "bookmark-controls";
+  
+    setBookmarkAttributes("play", onPlay, controlsElement);
+    setBookmarkAttributes("delete", onDelete, controlsElement);
+  
+    newBookmarkElement.id = "bookmark-" + bookmark.time;
+    newBookmarkElement.className = "bookmark";
+    newBookmarkElement.setAttribute("timestamp", bookmark.time);
+  
+    newBookmarkElement.appendChild(bookmarkTitleElement);
+    newBookmarkElement.appendChild(controlsElement);
+    bookmarks.appendChild(newBookmarkElement);
+};
 
 const viewBookmarks = () => {};
 
